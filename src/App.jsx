19730198a24c7e67dd0f7890ae69582ffe4d5231
laurence1Mas <119684303+laurence1Mas@users.noreply.github.com>
2026-02-07 17:04:54 +0100

    Making base structure of the project (#49)
    * chore: install Tailwind CSS, shadcn UI, and @vitejs/plugin-react
    
    * feat: configuration of the file /.github/CODEOWNERS to make approvals pull request
    
    * feat: The first tructure of the folders for the project
    
    * fix: Update Header and Footer section to make the structure of the component
    
    * fix: import style sheet in App

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/layouts/Header";
+import "./style.css";
+
+function App() {
+    return (
+        <div className="flex min-h-screen flex-col">
+
+            <Header />
+            <main className="flex-1">
+
+            </main>
+        </div>
+    );
+}
+export default App;
